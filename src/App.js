172ea9e66d2f995375d16bcc998143b6d86ecb8e
Joshua <joshuamayowa23@yahoo.com>
2023-01-05 15:14:55 +0100

    Patient Number Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { PatientNumber } from './pages/patientNumber/PatientNumber';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path='/'>
         <Route index element={<Home />}/>
+        <Route path='/pnum' element={<PatientNumber />}/>
       </Route>
     </Routes>
     </BrowserRouter>
--- src/pages/patientNumber/PatientNumber.jsx
+++ src/pages/patientNumber/PatientNumber.jsx
@@ -1,7 +1,21 @@
-import './PatientNumber.css';
+import { Button } from 'antd';
+import classes from './PatientNumber.module.css';
 
 export const PatientNumber = () => {
   return (
-    <div>PatientNumber</div>
+    <div className={classes.patientNumber}>
+      <div className={classes.patientNumberContainer}>        
+        <h2 className={classes.patientNumberTitle}>
+          Patient Number
+        </h2>
+        <p className={classes.patientNumberDesc}>
+          Enter the patient number to view all bill invoice under the patient’s name and make payments too
+        </p>
+        <form className={classes.patientNumberForm}>
+          <input type="num" placeholder='Enter invoice number' className={classes.patientNumberInput} />
+          <Button type='primary' className={classes.patientNumberBtn} block>Submit</Button>
+        </form>
+      </div>
+    </div>
   )
 }
