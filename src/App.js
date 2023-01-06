0ec3d83d8745d447e4618e99273f699fec791f50
Joshua <joshuamayowa23@yahoo.com>
2023-01-06 15:32:41 +0100

    outstanding invoice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { OutstandingInvoice } from './pages/outstandingInvoice/OutstandingInvoice';
 import { PatientNumber } from './pages/patientNumber/PatientNumber';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path='/'>
         <Route index element={<Home />}/>
         <Route path='/pnum' element={<PatientNumber />}/>
+        <Route path='/outinvoice' element={<OutstandingInvoice />}/>
       </Route>
     </Routes>
     </BrowserRouter>
